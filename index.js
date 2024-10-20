a6a5100a1320a3077c0e40c2800d7e1018837884
RomanRoma220342 bc2024-4 <diachuk20060@gmail.com>
2024-10-20 19:23:57 +0300

    Написав 1 частину трохи краще

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const { program } = require('commander');
-const fs = require('fs');
-
+const fs = require('fs').promises;
+const url = require('url');
 
 program
     .requiredOption('-h, --host <type>', 'адреса сервера')
@@ -10,28 +10,53 @@ program
 
 program.configureOutput({
     writeErr: (str) => {
-        console.error("Не задано обов'язкові параметри '-h (--host), -p(--port), -c(cache <path>)'");
+        console.error("Не задано обов'язкові параметри '-h (--host), -p (--port), -c (--cache <path>)'");
         process.exit(1);
     }
 });
 
 program.parse(process.argv);
 
-
 const options = program.opts();
 const host = options.host;
 const port = options.port;
-const cachePath = options.cache; 
+const cachePath = options.cache;
+
+const routes = {
+    '/health': {
+        'GET': (req, res) => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Юxyy!! Сервер паше!!!' }));
+        },
+    },
+};
 
 // Обробка запиту
 const requestListener = function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('Юxyy!! Сервер паше!!!');
+   try{
+
+    const parsedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+    const handler = routes[parsedUrl.pathname]?.[req.method];
+
+    if (handler) { // Якщо handler існує, викликати його
+        handler(req, res);
+    } else {
+        // Відповідь на невідомі маршрути
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Не знайдено' }));
+    }
+
+   } catch (err){
+    console.error(err)
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Помилка сервера' }));
+   }
+    
 };
 
 // Створення HTTP сервера
 const server = http.createServer(requestListener);
 
 server.listen(port, host, () => {
-    console.log(`Сервер стартанув на http://${host}:${port}`);
+    console.log(`Сервер стартанув на http://${host}:${port}/health`);
 });
